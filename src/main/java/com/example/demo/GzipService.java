ca8b76a2d5a56991b64aedf5856309d04c137d5d
Kostyantyn Panchenko <yougetit@ukr.net>
2019-01-10 23:51:37 +0200

    Refactored to code reuse

--- src/main/java/com/example/demo/GzipService.java
+++ src/main/java/com/example/demo/GzipService.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.zip.GZIPInputStream;
 import java.util.zip.GZIPOutputStream;
 
@@ -12,27 +14,27 @@ import org.springframework.stereotype.Service;
 @Service
 public class GzipService {
 
-    public File compress(File file) throws IOException {
-        String gzipFile = file.getAbsolutePath() + ".gz";
-        try (FileInputStream fis = new FileInputStream(file); GZIPOutputStream gzipOS = new GZIPOutputStream(new FileOutputStream(gzipFile))) {
-            byte[] buffer = new byte[1024];
-            int len;
-            while ((len = fis.read(buffer)) != -1) {
-                gzipOS.write(buffer, 0, len);
-            }
+    public File compress(final File file) throws IOException {
+        final String gzipFile = file.getAbsolutePath() + ".gz";
+        try (final FileInputStream fis = new FileInputStream(file); final GZIPOutputStream gzipOS = new GZIPOutputStream(new FileOutputStream(gzipFile))) {
+            write(fis, gzipOS);
         }
         return new File(gzipFile);
     }
 
-    public File uncompress(File gzipFile) throws IOException {
-        String file = gzipFile.getAbsolutePath().substring(0, gzipFile.getAbsolutePath().lastIndexOf(".")) + "2";
-        try (FileOutputStream fos = new FileOutputStream(file); GZIPInputStream gzipIS = new GZIPInputStream(new FileInputStream(gzipFile))) {
-            byte[] buffer = new byte[1024];
-            int len;
-            while ((len = gzipIS.read(buffer)) != -1) {
-                fos.write(buffer, 0, len);
-            }
+    public File uncompress(final File gzipFile) throws IOException {
+        final String file = gzipFile.getAbsolutePath().substring(0, gzipFile.getAbsolutePath().lastIndexOf(".")) + "2";
+        try (final FileOutputStream fos = new FileOutputStream(file); final GZIPInputStream gzipIS = new GZIPInputStream(new FileInputStream(gzipFile))) {
+            write(gzipIS, fos);
         }
         return new File(file);
+    }
+
+    private void write(final InputStream is, final OutputStream os) throws IOException {
+        final byte[] buffer = new byte[1024];
+        int len;
+        while ((len = is.read(buffer)) != -1) {
+            os.write(buffer, 0, len);
+        }
     }
 }
